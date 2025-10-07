e0aca5de7b50656834dca1720f57280f454c0005
CeleghinRiccardo <celeghin.salesianisesto@studenti.salesianisesto.it>
2025-10-07 13:23:13 +0200

    Secondo Commit

--- src/Box.java
+++ src/Box.java
@@ -1,9 +1,9 @@
-public class Box {
+import java.util.Scanner;
 
+public class Box {
+    public Scanner sc = new Scanner(System.in);
     private String tipoCont, nome;
     private int spazio;
-
-
 
 
 
@@ -13,6 +13,33 @@ public class Box {
         this.spazio=spazio;
     }
 
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getSpazio() {
+        return spazio;
+    }
+
+    public void setSpazio(int spazio) {
+        this.spazio = spazio;
+    }
+
+    public String getTipoCont() {
+        return tipoCont;
+    }
+
+    public void setTipoCont(String tipoCont) {
+        this.tipoCont = tipoCont;
+    }
+
+    public void scegliSpazio(){
+        System.out.println("quanto vuoi fare grandi i box?");
+        this.spazio=sc.nextInt();
+    }
 
 }
--- src/Magazzino.java
+++ src/Magazzino.java
@@ -1,3 +1,18 @@
 public class Magazzino {
+    private String nome;
 
+    private Box[] ListaBox = new Box[4];
+
+    public Magazzino(Box[] ListaBox){
+        this.ListaBox=ListaBox;
+        this.nome=nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = "Magazzino";
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
