ae22ccf5161f091b1a2ead34971efab33c24e7b5
CeleghinRiccardo <celeghin.salesianisesto@studenti.salesianisesto.it>
2025-10-07 12:36:09 +0200

    Initial commit

---
+++ src/Box.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+public class Box {
+
+    private String tipoCont, nome;
+    private int spazio;
+
+
+    public Box(String nome, String tipoCont, int spazio){
+        this.nome=nome;
+        this.tipoCont=tipoCont;
+        this.spazio=spazio;
+    }
+
+
+
+}
