10350121a8e2119439bd8bb7ab079b83e48257d0
CeleghinRiccardo <celeghin.salesianisesto@studenti.salesianisesto.it>
2025-10-07 12:52:36 +0200

    Primo Commit con Classi

--- src/Box.java
+++ src/Box.java
@@ -1,9 +1,10 @@
-import java.awt.*;
-
 public class Box {
 
     private String tipoCont, nome;
     private int spazio;
+
+
+
 
 
     public Box(String nome, String tipoCont, int spazio){
--- src/Oggetto.java
+++ src/Oggetto.java
@@ -1,2 +1,13 @@
 public class Oggetto {
+
+    private String tipo, nome;
+    private int peso;
+
+    public Oggetto(String tipo, String nome, int peso){
+        this.nome=nome;
+        this.peso=peso;
+        this.tipo=tipo;
+    }
+
+
 }
